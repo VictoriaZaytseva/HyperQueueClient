fb483aa53d4628169a6bda23ccf8b837f082f3fd
VictoriaZaytseva <vika01011001@gmail.com>
2016-06-03 00:33:26 -0400

    creating json in the js

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -1,15 +1,22 @@
+
+
 if (window.console) {
-  
   console.log("Welcome to your Play application's JavaScript!");
 }
+
 $(document).ready(function () {
 
   $("#consume-button").on("click", function () {
+    console.log($("#topic-consumer").val())
     console.log("consuming!")
   })
 
   $("#produce-button").on("click", function () {
-    console.log("producing!")
+
+    console.log($("#topic-producer").val())
+
+    var data = JSON.stringify({"name": $("#event-name").val(), "description": $("#event-description").val()})
+    console.log(data)
   })
 
 })
